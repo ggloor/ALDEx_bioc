d7b62c92f64f99e21f0ad9a55c2f3c914f4c680c
ggloor <ggloor@uwo.ca>
2019-10-26 15:22:49 -0400

    effect size density and E(dist)
    Brandon Lieng added functions to show effect size density plots. aldex.plotFeature, aldex.expectedDistance

--- R/expectedDistance.aldex.r
+++ R/expectedDistance.aldex.r
@@ -1,14 +1,14 @@
-aldex.expectedDistance <- function(d.clr) {
-    if (class(d.clr) != "aldex.clr" && class(d.clr) != "list") {
+aldex.expectedDistance <- function(clrData) {
+    if (class(clrData) != "aldex.clr" && class(clrData) != "list") {
         stop("Please supply a valid aldex.clr object.")
     }
 
-    d.mc <- getMonteCarloInstances(d.clr)
-    distances <- array(0, dim=c(length(getSampleIDs(d.clr)), length(getSampleIDs(d.clr)), d.clr@mc.samples))
+    d.mc <- getMonteCarloInstances(clrData)
+    distances <- array(0, dim=c(length(getSampleIDs(clrData)), length(getSampleIDs(clrData)), clrData@mc.samples))
 
     message("computing distances for each instance...")
     # Transpose call makes each row a sample and each col a feature for dist()
-    for (i in 1:d.clr@mc.samples) {
+    for (i in 1:clrData@mc.samples) {
         distances[,,i] <- distances[,,i] + as.matrix(dist(t(sapply(d.mc, function(x) {x[,i]}))))
     }
 
@@ -16,8 +16,8 @@ aldex.expectedDistance <- function(d.clr) {
     message("computing median distance across instances...")
     expectedDist <- as.matrix(apply(distances, c(1, 2), median))
 
-    rownames(expectedDist) <- getSampleIDs(d.clr)
-    colnames(expectedDist) <- getSampleIDs(d.clr)
-    
+    rownames(expectedDist) <- getSampleIDs(clrData)
+    colnames(expectedDist) <- getSampleIDs(clrData)
+
     return(as.dist(expectedDist))
 }
