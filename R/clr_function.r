73d381b550357d53df3950e797cda5a724d769ac
ggloor <ggloor@uwo.ca>
2022-03-28 10:48:22 -0400

    scale.samples takes a vector of same length as conds
    this is for the case where you anticipate each group has different scales

--- R/clr_function.r
+++ R/clr_function.r
@@ -188,14 +188,15 @@ if (verbose == TRUE) message("dirichlet samples complete")
           scale.samples = rbind(scale.samples, scale_for_sample) # archive these for output in clr object.
         }
         scale.samples <- scale.samples[-1,]
-      } else if(length(scale.samples) >1 & is.null(dim(scale.samples))){ ##Vector case/scale sim + senstitivity
-        warning("A vector was supplied for scale.samples. To run a sensitivity analysis, use 'aldex.senAnalysis()'.")
-        warning("Using only the first item in vector for scale simulation.")
-        lambda <- scale.samples[1]
+      } else if(length(scale.samples) == length(conds) & is.null(dim(scale.samples))){ ##Vector case/scale sim + senstitivity
+        message('a vector was supplied for scale samples')
+        #warning("A vector was supplied for scale.samples. To run a sensitivity analysis, use 'aldex.senAnalysis()'.")
+        #warning("Using only the first item in vector for scale simulation.")
+        lambda <- scale.samples
         scale.samples <-matrix(ncol = mc.samples)
         for(i in 1:length(p)){
           gm_sample <- log(apply(p[[i]],2,gm))
-          scale_for_sample <- sapply(gm_sample, FUN = function(mu){stats::rlnorm(1, mu, lambda)})
+          scale_for_sample <- sapply(gm_sample, FUN = function(mu){stats::rlnorm(1, mu, lambda[i])})
           l2p[[i]] <- sweep(log2(p[[i]]), 2,  log2(scale_for_sample), "-")
           scale.samples = rbind(scale.samples, scale_for_sample)
         }
